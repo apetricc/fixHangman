d2f4f7b339a8d2e26ea8d9a5cad5e3671466ec79
Andrew Petriccione <apetricc@unca.edu>
2016-12-10 12:23:58 -0500

    Put everything into functions including setting up the alphabet grid and creating all the event listeners

--- main.lua
+++ main.lua
@@ -27,6 +27,16 @@
   local correctGuesses = 0
   local winnerText
   local gameOver
+  local index
+  local outputX = 100
+  magicCopy = {}
+  printOut = {}
+  local magicX = 50
+  alphabet = {}
+  alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
+  "Q","R","S","T","U","V","W","X","Y","Z"}
+  local resetButton
+
 --stuff for the composer
   --composer.removeScene("hangman")
   --composer.gotoScene( "hangman" )
@@ -65,12 +75,12 @@
 
     local hangmanGroup = display.newGroup()
     local options = {
-    width = 315,
-    height = 315,
-    numFrames = 9,
-    sheetContentWidth = 945,
-    sheetContentHeight = 945
-  }
+        width = 315,
+        height = 315,
+        numFrames = 9,
+        sheetContentWidth = 945,
+        sheetContentHeight = 945
+    }
 
 
   local imageSheet = graphics.newImageSheet("images/sprite.png", options)
@@ -91,40 +101,29 @@
   hangman:setSequence("post")
   hangman:play()
   --initialize the alphabet of the actual letter strings
-  alphabet = {}
-  alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
-  "Q","R","S","T","U","V","W","X","Y","Z"}
 
   --******************************************************************************
   --CREATE MAGICWORD DISPLAY GRID
   --change alpha to 1 when letters selected from alphabet
   --******************************************************************************
-  magicCopy = {}
-  printOut = {}
-  local magicX = 50
+
   --local result
-
-
-  for i = 1, string.len(magicword), 1 do
-  	magicCopy[i] = string.sub(magicword, i, i)
-  	printOut[i] = display.newText(magicword:sub(i,i), magicX, 255, "drewsFont.ttf", 35 )
-  	lines[i] = display.newLine(magicX-15, 273, magicX+15, 273)
+--create the display objects for correctly guessed letters and the lines
+--to indicate how many letters are in the magicword
+local function createMagicWordPrintOut( i )
+    magicCopy[i] = string.sub(magicword, i, i)
+    printOut[i] = display.newText(magicword:sub(i,i), magicX, 255, "drewsFont.ttf", 35 )
+    lines[i] = display.newLine(magicX-15, 273, magicX+15, 273)
     lines[i]:setStrokeColor(1,0,0)
     lines[i].strokeWidth = 3
-
     magicX = magicX + 35
-  	printOut[i].alpha = 0
+    printOut[i].alpha = 0
+end
 
+  for i = 1, string.len(magicword), 1 do
+      createMagicWordPrintOut( i )
   end--for length of magicword add display text objects
 
-  local resetButton
-
---all this was broken
-  -- local function reset()
-  --   local resetButton = display.newImageRect( "images/resetButton.png",100,50 )
-  --   resetButton.x = display.contentCenterX
-  --   resetButton.y = display.contentCenterY +50
-  --   end--reset()
 
 
 
@@ -188,12 +187,11 @@ end--fillWord( letter )
 --checkIfWon checks if all the letters in the magicword have been correctly
 --guessed and if so, called winGame()
 function checkIfWon()
-
   for i = 1, table.getn(printOut), 1 do
         if (printOut[i].alpha == 1) then
-          correctGuesses = correctGuesses + 1
+            correctGuesses = correctGuesses + 1
         end
-        --ifWinner()
+
         if (correctGuesses == table.getn(printOut)) then
             winGame()
         end
@@ -205,78 +203,81 @@ end--checkIfWon()
 function testMatches( i )
   for j = 1, string.len(magicword),1 do
     if (alphabet[i]==string.sub(magicword,j,j)) then
-      fillWord( j )
-      checkIfWon()
-
+        fillWord( j )
+        checkIfWon()
     end --if letter matches index j of magicword
   end--for j, length of magicword
 end--testMatches()
 
 
 local function hangingTheMan()
-  hangman:setSequence(hangmanFrames[hangCount])
-  hangman:play()
-  --hangman[hangCount].alpha = 1
-  hangCount = hangCount + 1
-  print ("Hang count is: "..hangCount)
+    hangman:setSequence(hangmanFrames[hangCount])
+    hangman:play()
+    --hangman[hangCount].alpha = 1
+    hangCount = hangCount + 1
+    print ("Hang count is: "..hangCount)
 
 end
 
-local function displayGameOver()
+local function gameOverFunction()
     gameOver = display.newText("GAME OVER!", display.contentCenterX,
     display.contentCenterY, "drewsFont.ttf", 55)
     gameOver:setFillColor(1,0,0)
     reset()
 end
+
+local function createLetter( i )
+    newLetter[i] = display.newText(alphabet[i], startLetters, yCoord,
+    "drewsFont.ttf", 35 )
+    newLetter[i]:setFillColor( 0, 0, 0 )
+    newLetter[i].alpha = .25
+    outputX = 100
+end
+
+local function updateYCoord( i )
+    if (i % 7 == 0 ) then
+         yCoord = yCoord + 40
+         startLetters = initialX
+    end
+end
+
+local function createLettersAndTheirEventListeners()
+    for i = 1, 26, 1 do
+        updateYCoord( i )
+        createLetter( i )
+
+        local function letterListener( event )
+
+            if ( event.phase == "began" and newLetter[i].alpha ~= 1 and hangCount < 8 and hasWon==false) then
+                    fillLetters(i)
+
+                  if (string.find(magicword,alphabet[i])~=nil) then
+                      testMatches( i )
+
+                  elseif (hangCount < 7) then
+                      hangingTheMan()
+
+                  if (hangCount > 6 and gameOver==nil) then
+                      gameOverFunction()
+                  end--if hangCount > 6, gameOver
+
+                end--end if matches a letter in magicword, hangCount < 8
+              end--if event began
+
+              return true
+        end--letterListener( event )
+
+        newLetter[i]:addEventListener( "touch", letterListener )
+        startLetters = startLetters + 30
+
+    end--for length of alphabet
+
+end
   --******************************************************************************
   --CREATE ALPHABET GRID for touch interaction with copy,
   --then compare original array to magic word
   --******************************************************************************
-
-  --for loop to set alphabet letters on screen with event listeners
-  for i = 1, 26, 1 do
-  		 if (i % 7 == 0 ) then
-  			 	yCoord = yCoord + 40
-  			 	startLetters = initialX
-  		 end
-
-  		newLetter[i] = display.newText(alphabet[i], startLetters, yCoord,
-			"drewsFont.ttf", 35 )
-  		newLetter[i]:setFillColor( 0, 0, 0 )
-  		newLetter[i].alpha = .25
-  		local index
-
-  		local outputX = 100
-  		local function letterListener( event )
-
-  				if ( event.phase == "began" and newLetter[i].alpha ~= 1 and hangCount < 8 and hasWon==false) then
-                  fillLetters(i)
-
-  					    if (string.find(magicword,alphabet[i])~=nil) then
-                    testMatches( i )
-
-  					    elseif (hangCount < 7) then
-                    hangingTheMan()
-
-                if (hangCount > 6 and gameOver==nil) then
-					    		  -- gameOver = display.newText("GAME OVER!", display.contentCenterX,
-					    			-- display.contentCenterY, "drewsFont.ttf", 55)
-					    		  -- gameOver:setFillColor(1,0,0)
-                    -- reset()
-                    displayGameOver()
-					    	end--if hangCount > 6, gameOver
-
-              end--end if matches a letter in magicword, hangCount < 8
-  			    end--if event began
-
-  			    return true
-  		end--letterListener( event )
-
-  		newLetter[i]:addEventListener( "touch", letterListener )
-  		startLetters = startLetters + 30
-
-  end--for length of alphabet
-
+createLettersAndTheirEventListeners()
 
 
 --end--scene:create(event)
